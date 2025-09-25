b9d893760d752adad3d2e6ea75f73798a0e2b223
sangmin <sangsang366@gmail.com>
2025-09-25 13:55:32 +0900

    take damage 함수 오버라이딩, enemy bp 데미지 파이프라인 수정, 사망 시 wavemanager monster death 호출

--- Source/DroneDefense/Private/EnemyBase.cpp
+++ Source/DroneDefense/Private/EnemyBase.cpp
@@ -2,6 +2,9 @@
 #include "WaveManager.h"
 #include "TimerManager.h"
 #include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+#include "WaveManager.h"
+
 
 // Sets default values
 AEnemyBase::AEnemyBase()
@@ -14,7 +17,6 @@ void AEnemyBase::BeginPlay()
 {
     Super::BeginPlay();
     HP = Max_HP;
-
     CalculateValue();
 
     if (GetWorld() && _isDestroyCount)
@@ -26,6 +28,16 @@ void AEnemyBase::BeginPlay()
             _destroyDelay,
             false
         );
+    }
+
+    //레벨에서 WaveManager 액터 찾기
+    TArray<AActor*> FoundActors;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), AWaveManager::StaticClass(), FoundActors);
+
+    if (FoundActors.Num() > 0)
+    {
+        WaveManagerActor = Cast<AWaveManager>(FoundActors[0]); // 첫 번째 액터 사용
+        UE_LOG(LogTemp, Display, TEXT("Found Wave manager"));
     }
 }
 
@@ -46,23 +58,58 @@ void AEnemyBase::DestroySelf()
         _playerController->MonsterDeath();
     }
 
-    Destroy();
+    // MonsterDeath 호출
+    if (WaveManagerActor)
+    {
+        WaveManagerActor->MonsterDeath();
+    }
+
+
+    if (!GetWorldTimerManager().IsTimerActive(DestroyTimerHandle))
+        GetWorldTimerManager().SetTimer(DestroyTimerHandle, [this]()
+            {
+                Destroy();
+            }, 2.0f, false);
 }
 
-void AEnemyBase::TakeDamage(float Damage)
+bool AEnemyBase::TakeDamage_Implementation(float Damage)
 {
-    GotHit.Broadcast();
+    
+    //GotHit.Broadcast();
+    if (HP <= 0) return false;
+
     HP -= Damage;
-    Hit = true;
-    UE_LOG(LogTemp, Warning, TEXT("HP : %d"), HP);
     if (HP <= 0)
     {
-        if (!GetWorldTimerManager().IsTimerActive(DestroyTimerHandle))
-         GetWorldTimerManager().SetTimer(DestroyTimerHandle, this, &AEnemyBase::DestroySelf, 2.0f, false);
+        Dead = true;
+
+        DestroySelf();
     }
+    /*else
+    {
+        Hit = true;//피격 시
+        if (MonsterWidget)
+            MonsterWidget->SetVisibility(false);//위젯 컴포넌트 숨기기
+        GetWorldTimerManager().SetTimer(
+            HitTimerHandle,
+            [this]()   // 2초 뒤 실행
+            {
+                if (MonsterWidget)
+                    MonsterWidget->SetVisibility(true);//위젯 컴포넌트 보이기
+            },
+            2.0f,
+            false
+        );
+    }*/
+    return true;
 }
 
 void AEnemyBase::ToggleClose()
 {
      GotClose.Broadcast();
 }
+
+void AEnemyBase::ToggleHit()
+{
+    GotHit.Broadcast();
+};
